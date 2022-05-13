629044721166249e76f3a23ae96b05c5edc9bec5
Bruno Martins <bruno.martins217@gmail.com>
2022-05-13 20:06:47 -0300

    feat: post categories route

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,10 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.post('/credit', (req, res) => {
-  const { money } = req.body;
-  return res.json({ money })
-})
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("Server On!"));
