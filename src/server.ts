429ef97a47a0b7c58bc9ba0dcd4f43c6c786b655
Bruno Martins <bruno.martins217@gmail.com>
2022-05-13 19:17:00 -0300

    setup vscode debug

--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,9 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.post('/credit', (req, res) => {
+  const { money } = req.body;
+  return res.json({ money })
+})
+
 app.listen(3333, () => console.log("Server On!"));
