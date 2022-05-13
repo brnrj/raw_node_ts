04ea6a89919548d33fd319047f1dd60e3bfa8320
Bruno Martins <bruno.martins217@gmail.com>
2022-05-13 20:07:14 -0300

    tracking files;

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import { v4 as uuidv4 } from "uuid";
+
+const categoriesRoutes = Router();
+
+const categories = [];
+
+categoriesRoutes.post("/", (req, res) => {
+  const { name, description } = req.body;
+
+  const category = {
+    name,
+    description,
+    id: uuidv4()
+  }
+
+  categories.push(category);
+
+  return res.status(201).json({ name, description });
+});
+
+export { categoriesRoutes };
