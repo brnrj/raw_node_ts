b17df18570fa50982c3870141e5af2da3efdc36e
Bruno Martins <bruno.martins217@gmail.com>
2022-05-14 19:14:06 -0300

    feat: routes index file

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,8 +1,6 @@
 import { Router } from "express";
 import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
-
-
 const specificationsRoutes = Router();
 
 specificationsRoutes.post("/", (req, res) => {
