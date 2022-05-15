fa4fbd94640cfe96cb456f2849be2c054e090e71
Bruno Martins <bruno.martins217@gmail.com>
2022-05-15 12:01:25 -0300

    feat: first migration config

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import { DataSource } from "typeorm";
 
 const dataSource = new DataSource({
@@ -6,7 +7,16 @@ const dataSource = new DataSource({
   port: 5432,
   username: "docker",
   password: "firstapi",
-  database: "remtx"
+  database: "remtx",
+  synchronize: true,
+  logging: true,
+  migrations: ["./src/database/migrations/*.ts"],
+  entities: [],
+  subscribers: []
 });
 
-dataSource.initialize();
+export function createConnection(host = "database"): Promise<DataSource> {
+  return dataSource.setOptions({ host }).initialize();
+}
+
+export default dataSource;
